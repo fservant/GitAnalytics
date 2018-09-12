27db2c2eebce1f195edb0af6acc7d81bd1fb5cdd
akherbst2 <alyssa2@vt.edu>
2018-09-12 14:18:49 -0400

    Webpage on local server
    I added node capabilities that let you put index.html on your local server.
    
    1. run 'node server.js'
    2. Open a web browser.  Go to URL 'localhost:3000'
    
    You should see the html page (no style page yet) on the web page.

---
+++ CI/Site/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+var path = require('path')
+//app.set('view engine', 'html')
+
+app.get('/', function (req, res) {
+  //res.send('Hello World!')
+  //res.render('index')
+  res.sendFile(path.join(__dirname+'/index.html'))
+})
+
+app.listen(3000, function () {
+  console.log('Git Analytics CI site listening on port 3000!')
+})
