ab9685de977f12ce87ba184f5139e72999ac3c6d
Dom Edwards <dedwards@qualtrax.com>
2018-09-24 14:41:42 -0400

    scripts pull from api and print to file

--- CI/Analysis/SimpleHistory.py
+++ CI/Analysis/SimpleHistory.py
@@ -1,9 +1,22 @@
-import json
-with open("rails50.json") as tweetfile:
-    pyresponse = json.load(tweetfile)
+import sys, json, requests
+token = sys.argv[5]
+owner = sys.argv[1]
+repo = sys.argv[2]
+builds_length = sys.argv[3]
+output = sys.argv[4]
+headers = {"Travis-API-Version": "3", "Authorization": "token "+token}
+
+r = requests.get("https://api.travis-ci.org/repo/" + owner + "%2F" + repo + "/builds?limit="+builds_length, headers=headers)
+
+if r.status_code != 200:
+    print("invalid request")
+    sys.exit(1)
+
+pyresponse = r.json()
+
 builds = pyresponse['builds']
 
 simple = [{'number':b.get('number'), "hash":b.get('commit').get('sha')[:8], 'owner':b.get('created_by').get('login'), 'time':b.get('started_at'), 'status':b.get('state')} for b in builds]
 
-with open('railsHistory.json', 'w') as outfile:
+with open(output, 'w') as outfile:
     json.dump(simple, outfile, indent=4)
