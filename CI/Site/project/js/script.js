909ee1f2c3c35fd05a0dabdec92d49b7b32a3cd8
akherbst2 <alyssa2@vt.edu>
2018-09-26 15:30:23 -0400

    Same Table BuT dYnAmIc!!
    Same table but a lil slower (:

--- CI/Site/project/js/script.js
+++ CI/Site/project/js/script.js
@@ -3,7 +3,7 @@ function loadJSON(callback) {
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'rails50analysis.json', true); // Replace 'my_data' with the path to your file
+    xobj.open('GET', 'rails50SimpleHistory.json', true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
@@ -13,38 +13,40 @@ function loadJSON(callback) {
     xobj.send(null);
 }
 
-function tableCreate() {
+function tableCreate(blob) {
     var tbl = document.getElementById('builds');
     var tbdy = document.createElement('tbody');
     var tr = document.createElement('tr');
-    loadJSON(function(response) {
-        var actual_json = JSON.parse(response)
-        console.log(actual_json)
-    })
-    for(var i = 0; i < 3; i++) {
+
+    for(var i = 0; i < blob.length; i++) {
         var tr = document.createElement('tr');
             var td = document.createElement('td');
-            td.appendChild(document.createTextNode('1234'));
+            td.appendChild(document.createTextNode(blob[i].number));
+            td.setAttribute('align', 'right')
             tr.appendChild(td);
 
             td = document.createElement('td');
-            td.appendChild(document.createTextNode('hashcode here'));
+            td.appendChild(document.createTextNode(blob[i].hash));
             tr.appendChild(td);
 
             td = document.createElement('td');
-            td.appendChild(document.createTextNode('alyssa'));
+            td.appendChild(document.createTextNode(blob[i].owner));
             tr.appendChild(td);
 
             td = document.createElement('td');
-            td.appendChild(document.createTextNode('12:45'));
+            td.appendChild(document.createTextNode(blob[i].time));
             tr.appendChild(td);
 
             td = document.createElement('td');
-            td.appendChild(document.createTextNode('passed'));
+            td.appendChild(document.createTextNode(blob[i].status));
             tr.appendChild(td);
         tbdy.appendChild(tr);
     }
     tbdy.append(tr);
     tbl.appendChild(tbdy);
 }
+loadJSON(function(response) {
+    var actual_json = JSON.parse(response);
+    tableCreate(actual_json);
+});
 window.onload = tableCreate;
