a26e4178ffb0b473c9289ee6ddb8c3f24ae91bab
akherbst2 <alyssa2@vt.edu>
2018-09-26 14:52:19 -0400

    Access JSON file through javascript
    I am able to access the JSON file and print it to the site console.  Next, we should parse the JSON data in the javascript file and put it into the table.

--- CI/Site/project/js/script.js
+++ CI/Site/project/js/script.js
@@ -1,9 +1,26 @@
 
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'rails50analysis.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
 function tableCreate() {
     var tbl = document.getElementById('builds');
     var tbdy = document.createElement('tbody');
     var tr = document.createElement('tr');
-
+    loadJSON(function(response) {
+        var actual_json = JSON.parse(response)
+        console.log(actual_json)
+    })
     for(var i = 0; i < 3; i++) {
         var tr = document.createElement('tr');
             var td = document.createElement('td');
