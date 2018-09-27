8a4b63802b176a66d8afcbdc00ddd7c7dad46205
Zuri Wong <zuriwong@Zuris-MacBook-Pro.local>
2018-09-27 11:35:37 -0400

    Added D3 generic graph

--- public/app.js
+++ public/app.js
@@ -33,6 +33,11 @@ angular.module('authorship', ['ngRoute'])
       })
     }
 
+    // var colors = ['#ff0000', '#00ff00', '#0000ff'];
+    // var random_color = colors[Math.floor(Math.random() * colors.length)];
+    // document.getElementById('title').style.color = random_color;
+
+
 
   })
 
