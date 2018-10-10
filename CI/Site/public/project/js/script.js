f3908785f2e08df38e50970b3fcc82e13bc66265
akherbst2 <alyssa2@vt.edu>
2018-10-10 17:57:27 -0400

    Dynamic Calendar
    Calendar is now rendered with javascript (not static html).

--- CI/Site/public/project/js/script.js
+++ CI/Site/public/project/js/script.js
@@ -1,53 +1,90 @@
 
+
 function tableCreate(blob) {
-    var tbl = document.getElementById('builds');
-    var tbdy = document.createElement('tbody');
+  var tbl = document.getElementById('builds');
+  var tbdy = document.createElement('tbody');
+  var tr = document.createElement('tr');
+  console.log(typeof blob);
+
+  for (let [_key, elem] of Object.entries(blob)) {
     var tr = document.createElement('tr');
-    console.log(typeof blob);
+    var td = document.createElement('td');
+    td.appendChild(document.createTextNode(elem.number));
+    td.setAttribute('align', 'right')
+    tr.appendChild(td);
 
-    for (let [_key, elem] of Object.entries(blob)) {
-      console.log(elem)
-      var tr = document.createElement('tr');
-      var td = document.createElement('td');
-      td.appendChild(document.createTextNode(elem.number));
-      td.setAttribute('align', 'right')
-      tr.appendChild(td);
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(elem.hash));
+    tr.appendChild(td);
 
-      td = document.createElement('td');
-      td.appendChild(document.createTextNode(elem.hash));
-      tr.appendChild(td);
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(elem.owner));
+    tr.appendChild(td);
 
-      td = document.createElement('td');
-      td.appendChild(document.createTextNode(elem.owner));
-      tr.appendChild(td);
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(elem.time));
+    tr.appendChild(td);
 
-      td = document.createElement('td');
-      td.appendChild(document.createTextNode(elem.time));
-      tr.appendChild(td);
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(elem.status));
+    tr.appendChild(td);
+    tbdy.appendChild(tr);
+  }
 
-      td = document.createElement('td');
-      td.appendChild(document.createTextNode(elem.status));
-      tr.appendChild(td);
-      tbdy.appendChild(tr);
-    }
-
-    tbdy.append(tr);
-    tbl.appendChild(tbdy);
+  tbdy.append(tr);
+  tbl.appendChild(tbdy);
+}
+function isValidDate(d) {
+  return Object.prototype.toString.call(d) === "[object Date]" && d instanceof Date && !isNaN(d);
 }
 
-function makePage() {
-    // Initialize Cloud Firestore through Firebase
-    var db = firebase.firestore();
-    // Disable deprecated features
-    db.settings({
-        timestampsInSnapshots: true
-    });
+function dateFromISOString(s) {
+  return new Date(new Date().setTime(Date.parse(s)));
+}
 
-    db.collection("history").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()[50554]}`);
-      console.log(doc.data()[50554])
-      tableCreate(doc.data())
+function dataToDateMap(data) {
+  let map = new Map();
+  Object.values(data).forEach((elem) => {
+    let from_date = dateFromISOString(elem.time);
+    if(isValidDate(from_date)) {
+      let utc = Date.UTC(from_date.getUTCFullYear(), from_date.getMonth(), from_date.getDay());
+      map.set(utc, (map.has(utc) ? map.get(utc) : 0) + 1);
+    }
+  });
+  let out_arr = [];
+  for(let [key, val] of map.entries()) {
+    out_arr.push([new Date(key), val]);
+  }
+  return out_arr;
+}
+
+function calendarCreate(data) {
+  let dataTable = new google.visualization.DataTable();
+  dataTable.addColumn({ type: 'date', id: 'Date' });
+  dataTable.addColumn({ type: 'number', id: 'Won/Loss' });
+  dataTable.addRows(dataToDateMap(data));
+  let chart = new google.visualization.Calendar(document.getElementById('calendar_basic'));
+  let options = {
+    title: "Builds By Date",
+    height: 300,
+  };
+  chart.draw(dataTable, options);
+}
+
+
+function makePage() {
+  // Initialize Cloud Firestore through Firebase
+  var db = firebase.firestore();
+  // Disable deprecated features
+  db.settings({
+    timestampsInSnapshots: true
+  });
+  google.charts.load("current", {packages:["calendar"]});
+
+  db.collection("history").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      tableCreate(doc.data());
+      google.charts.setOnLoadCallback(calendarCreate(doc.data()));
     });
   });
 }
