b93648d5181b71c6dbad62c7ebe552004544a60b
akherbst2 <alyssa2@vt.edu>
2018-10-10 15:23:29 -0400

    Load from firebase
    Loads historical rails data from firebase.

--- CI/Site/public/project/js/script.js
+++ CI/Site/public/project/js/script.js
@@ -1,61 +1,55 @@
 
-function loadJSON(callback) {
-
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'rails50SimpleHistory.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
-
 function tableCreate(blob) {
     var tbl = document.getElementById('builds');
     var tbdy = document.createElement('tbody');
     var tr = document.createElement('tr');
+    console.log(typeof blob);
 
-    for(var i = 0; i < blob.length; i++) {
-        var tr = document.createElement('tr');
-            var td = document.createElement('td');
-            td.appendChild(document.createTextNode(blob[i].number));
-            td.setAttribute('align', 'right')
-            tr.appendChild(td);
+    for (let [_key, elem] of Object.entries(blob)) {
+      console.log(elem)
+      var tr = document.createElement('tr');
+      var td = document.createElement('td');
+      td.appendChild(document.createTextNode(elem.number));
+      td.setAttribute('align', 'right')
+      tr.appendChild(td);
 
-            td = document.createElement('td');
-            td.appendChild(document.createTextNode(blob[i].hash));
-            tr.appendChild(td);
+      td = document.createElement('td');
+      td.appendChild(document.createTextNode(elem.hash));
+      tr.appendChild(td);
 
-            td = document.createElement('td');
-            td.appendChild(document.createTextNode(blob[i].owner));
-            tr.appendChild(td);
+      td = document.createElement('td');
+      td.appendChild(document.createTextNode(elem.owner));
+      tr.appendChild(td);
 
-            td = document.createElement('td');
-            td.appendChild(document.createTextNode(blob[i].time));
-            tr.appendChild(td);
+      td = document.createElement('td');
+      td.appendChild(document.createTextNode(elem.time));
+      tr.appendChild(td);
 
-            td = document.createElement('td');
-            td.appendChild(document.createTextNode(blob[i].status));
-            tr.appendChild(td);
-        tbdy.appendChild(tr);
+      td = document.createElement('td');
+      td.appendChild(document.createTextNode(elem.status));
+      tr.appendChild(td);
+      tbdy.appendChild(tr);
     }
+
     tbdy.append(tr);
     tbl.appendChild(tbdy);
 }
 
-
-function drawChart() {
-
-}
-loadJSON(function(response) {
-    var actual_json = JSON.parse(response);
-    tableCreate(actual_json);
-});
-
 function makePage() {
-    drawChart();
+    // Initialize Cloud Firestore through Firebase
+    var db = firebase.firestore();
+    // Disable deprecated features
+    db.settings({
+        timestampsInSnapshots: true
+    });
+
+    db.collection("history").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()[50554]}`);
+      console.log(doc.data()[50554])
+      tableCreate(doc.data())
+    });
+  });
 }
+
 window.onload = makePage;
