7fcde42c8017f171587ea2cf8558b7b23c9b73f2
puyush <puyushgoel@gmail.com>
2018-10-30 16:06:09 -0400

    split the patches to get them ready for rendering

--- deepHistory/src/app/history/history.component.ts
+++ deepHistory/src/app/history/history.component.ts
@@ -6,38 +6,46 @@ import { RepoSharedService } from "../services/repo-shared-service";
 import { HttpClient } from "@angular/common/http";
 
 @Component({
-    selector: "page-history",
-    templateUrl: "history.component.html",
-    styleUrls: ["history.component.css"]
-  })
-  export class HistoryComponent implements OnInit {
-    loginName: string;
-    repoName: string;
-    commitIndex: number;
-    commitSha: string;
-    fileName: string;
-    filePatches: string[];
+  selector: "page-history",
+  templateUrl: "history.component.html",
+  styleUrls: ["history.component.css"]
+})
+export class HistoryComponent implements OnInit {
+  loginName: string;
+  repoName: string;
+  commitIndex: number;
+  commitSha: string;
+  fileName: string;
+  filePatches: string[];
+  patches: string[][];
 
-    githubApiService: GithubApiService = new GithubApiService(this.httpClient);
+  githubApiService: GithubApiService = new GithubApiService(this.httpClient);
 
-    commits: any;
+  commits: any;
 
-    constructor(
-      private httpClient: HttpClient,
-      private router: Router,
-      private route: ActivatedRoute,     
-      private dataService: DataService,
-      private repoService: RepoSharedService) {
-        this.repoService.current.subscribe(name => (this.repoName = name));
-        this.dataService.current.subscribe(name => (this.loginName = name));
-    }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+    private route: ActivatedRoute,
+    private dataService: DataService,
+    private repoService: RepoSharedService
+  ) {
+    this.repoService.current.subscribe(name => (this.repoName = name));
+    this.dataService.current.subscribe(name => (this.loginName = name));
+  }
 
-    getDiffs(index: number, array: string[]): Promise<string[]> {
-      return new Promise((resolve, reject) => {
-        this.githubApiService
-        .getSingleCommitWithSha(this.loginName, this.repoName, this.commits[index]["sha"])
+  getDiffs(index: number, array: string[]): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      this.githubApiService
+        .getSingleCommitWithSha(
+          this.loginName,
+          this.repoName,
+          this.commits[index]["sha"]
+        )
         .forEach(res => {
-          let tmpIndex = res["files"].findIndex(file => file["filename"] == this.fileName);
+          let tmpIndex = res["files"].findIndex(
+            file => file["filename"] == this.fileName
+          );
           if (tmpIndex !== -1) {
             array.push(res["files"][tmpIndex]["patch"]);
           }
@@ -56,39 +64,46 @@ import { HttpClient } from "@angular/common/http";
             console.log("Error when getting diffs", err);
             reject(err);
           }
-        ); 
-      });
-    }
+        );
+    });
+  }
 
-    setup(): void {
-      this.filePatches = new Array<string>();
+  setup(): void {
+    this.filePatches = new Array<string>();
+    this.patches = new Array<Array<string>>();
 
-      this.route.params.subscribe(params => {
-        this.commitIndex = +params['commitNumber'];
-        this.commitSha = params['sha'];
-        this.fileName = params['filePath'];
-      });
-      
-      //gets all commits
-      this.githubApiService
-        .getRepositoryCommits(this.loginName, this.repoName)
-        .forEach((commit: any) => {
-          this.commits = commit;
-        })
-        .then(res => {
-          this.getDiffs(this.commitIndex, new Array<string>())
-            .then(res => {
-              this.filePatches = res;
-              console.log(res);
-            });
+    this.route.params.subscribe(params => {
+      this.commitIndex = +params["commitNumber"];
+      this.commitSha = params["sha"];
+      this.fileName = params["filePath"];
+    });
+
+    //gets all commits
+    this.githubApiService
+      .getRepositoryCommits(this.loginName, this.repoName)
+      .forEach((commit: any) => {
+        this.commits = commit;
+      })
+      .then(res => {
+        this.getDiffs(this.commitIndex, new Array<string>()).then(res => {
+          this.filePatches = res;
+          this.splitFilesPatches();
         });
-    }
+      });
+  }
 
-    ngOnInit(): void {
-      if (this.repoName !== "default name") {
-        this.setup();
-      } else {
-        this.router.navigate(["/user"]);
-      }
+  splitFilesPatches() {
+    this.filePatches.forEach(element => {
+      let patch = element.split("\n").map(item => item.trim());
+      this.patches.push(patch);
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.repoName !== "default name") {
+      this.setup();
+    } else {
+      this.router.navigate(["/user"]);
     }
   }
+}
